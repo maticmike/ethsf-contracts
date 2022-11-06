ed2bda2a3558f1a81d20e6b18e5e3a62a0bcba93
Joshua Moore <josh@eggheads.io>
2022-11-06 09:37:03 -0800

    remove some things

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,8 @@
 const {ethers, upgrades} = require("hardhat");
 const {t} = require("tar");
 
+const BN = (number) => ethers.BigNumber.from(number);
+
 async function main() {
   const provider = ethers.provider;
   const deployer = new ethers.Wallet(process.env.AURORA_PRIVATE_KEY, provider);
@@ -30,38 +32,43 @@ async function main() {
     "0x007A22900a3B98143368Bd5906f8E17e9867581b",
   ];
 
-  const TokenRenderer = await ethers.getContractFactory("TokenRenderer");
-  const tokenrenderer = await upgrades.deployProxy(TokenRenderer, [
-    erc20s,
-    tokenNames,
-    tokenColors,
-    aggregators,
-  ]);
-  await tokenrenderer.deployed();
+  // const TokenRenderer = await ethers.getContractFactory("TokenRenderer");
+  // const tokenrenderer = await upgrades.deployProxy(TokenRenderer, [erc20s, tokenNames, tokenColors, aggregators]);
+  // await tokenrenderer.deployed();
 
-  console.log("TokenRenderer address:", tokenrenderer.address);
+  // console.log("TokenRenderer address:", tokenrenderer.address);
 
   const beneficiary = deployer.address;
   const vestingDate = 1691650712;
 
-  const SoulFund = await ethers.getContractFactory("SoulFund");
+  const jurors = [
+    "0x6E4D602cC4893e1fa9Fb1BC702E9a2C37522fCc4",
+    "0xCb9f7FD7fBED49f94A6e68E419A1160Fe8DFaBbE",
+    "0x85C54e29c70b54072C2E6Bbc70e856d56Dd7002A",
+    "0x992419d318DE220cD87d036A535665C3405834C0",
+    "0xb25910F16bBFd86930d97Dae807b196B4D277e66",
+    "0x118E63656C67aD6E0a9BE1a3bFe6e5553182480E",
+  ];
+  const jurySwap = BN("86400");
+  const minJurySize = BN("3");
+  const deadline = BN(Math.trunc(Date.now() / 1000 + 86400).toString());
 
-  // For upgradeable version
-  const soulfund = await SoulFund.deploy(
-    beneficiary,
-    vestingDate,
-    tokenrenderer.address,
-    "0xBF62ef1486468a6bd26Dd669C06db43dEd5B849B",
-    "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6"
-  );
-  await soulfund.deployed();
+  const SoulFundFactory = await ethers.getContractFactory("SoulFundFactory");
+  const soulFundFactory = SoulFundFactory.attach("0xC22ab064A007B2E7c13d8F9559Bce9F09008C6d0");
+
+  let tx = await soulFundFactory.deployNewSoulFund(vestingDate, jurors, jurySwap, minJurySize);
+  let txReceipt = await tx.wait();
+  let newSoulFundTokenDeployedEvent = txReceipt.events?.filter((events) => events.event == "NewSoulFundTokenDeployed");
+
+  let sfaddress = newSoulFundTokenDeployedEvent[0].args.tokenAddress;
+
+  const SoulFund = await ethers.getContractFactory("SoulFund");
+  soulfund = SoulFund.attach(sfaddress);
 
   console.log("SoulFund address:", soulfund.address);
 
   let options = {value: ethers.utils.parseEther("0.01")};
-  let tx = await soulfund.safeMint(
-    "0x85C54e29c70b54072C2E6Bbc70e856d56Dd7002A"
-  );
+  tx = await soulfund.safeMint("0x85C54e29c70b54072C2E6Bbc70e856d56Dd7002A");
   await tx.wait(1);
   tx = await soulfund.depositFund(
     0,
